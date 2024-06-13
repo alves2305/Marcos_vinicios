f2114f1635c24e2a8c3a3d299074cab70b93331b
alves2305 <raffa.alves.souza@gmail.com>
2024-06-13 17:23:05 -0300

    projeto-v

---
+++ script.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+  });
+  
+  // Aplica o efeito às seções
+  sr.reveal('#section1', { delay: 200 });
+  sr.reveal('#section2', { delay: 400 });
+  sr.reveal('#section3', { delay: 600 });
+  sr.reveal('#section4', { delay: 800 });
